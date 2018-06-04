02bf44cf6a8d8acba0c92876a152b925ebf08eec
rawqing <kingstar95d@gmail.com>
2018-06-04 20:12:13 +0800

    添加失败重试

---
+++ milk_mini/src/main/java/com/jlc/app/milk_mini/bTest/InitTest.java
@@ -0,0 +1,23 @@
+package com.jlc.app.milk_mini.bTest;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.jlc.app.milk_mini.bTest.rules.Retry;
+
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class InitTest {
+
+    private ErrorCollector collector = new ErrorCollector();
+    private Retry retry = new Retry(3);
+    // 失败重试规则定义 ( 定义为3次 )
+    @Rule
+    public TestRule chain= RuleChain
+            . outerRule(collector)
+            . around(retry);
+}
